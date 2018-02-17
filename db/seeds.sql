7ee9d597e57f3ed7bb334fa78957b565081f3a19
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2018-02-17 10:05:34 -0500

    all files and folders, some sql and server

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('the gargantuan', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('the bazooka burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('the hot dog burger', FALSE);
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParse = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
+
+
+app.listen(PORT, function() {
+  console.log("Listening on port: ", PORT);
+});
